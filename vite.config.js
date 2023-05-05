13fb1b68e27323ce6048a9c7d9d93f967b698633
nbellana <nbellana@innominds.com>
2023-05-05 23:33:46 +0530

    configuration for production mode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({command, mode, ssrBuild}) => {
-    console.log(command, mode, ssrBuild);
+    // console.log(command, mode, ssrBuild);
+
+    //We can add base specific to mode. 
+    if(mode == "production"){ // now the local url will be like http://localhost:4173/vite/ when we run in production mode
+        return {
+            base: "/vite/"
+        }
+    }
 
     return{};
 })
